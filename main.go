7498af04c3712d2a060e961b4bc1310122eb2234
ananthvk <mailstoanantha@gmail.com>
2025-08-30 02:24:08 +0530

    feat: basic http server

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("GET /")
+	fmt.Fprintf(w, "%s", "Hello there")
+}
 
 func main() {
-	fmt.Println("hello world")
+	godotenv.Load()
+
+	host, ok := os.LookupEnv("HOST")
+	if !ok {
+		host = "127.0.0.1"
+	}
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8000"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homePageHandler)
+
+	server := &http.Server{
+		Addr:    host + ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Server listening on %s", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
