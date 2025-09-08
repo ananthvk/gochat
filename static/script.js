9621a6e352aaaf933cd316cd1aeeac84349fa6d4
ananthvk <mailstoanantha@gmail.com>
2025-09-08 20:51:08 +0530

    feat: fix scrolling issues

--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
 var socket = null;
 
+// Only scroll when the user is at the bottom of the messages list
+const SCROLL_MESSAGES_DISTANCE = 100;
+
 document.addEventListener("DOMContentLoaded", function (event) {
     socket = new WebSocket("ws://192.168.1.199:8000/api/v1/realtime/ws");
 
@@ -17,21 +20,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     socket.onmessage = event => {
         let text = event.data;
-        console.log(text)
-        console.log("GOT MESSAGE FROM SERVER", event)
-
-        let newNode = document.createElement("div")
-        newNode.classList.add("message", "message-other")
-        newNode.textContent = text
-
-        document.getElementById("messages").appendChild(newNode)
-
-        // TODO: Only scroll when the user is already at the end of the div, i.e. don't scroll if the user is reading a message up
-        const messagesDiv = document.getElementById('messages');
-        messagesDiv.scrollTo({
-            top: messagesDiv.scrollHeight,
-            behavior: 'smooth'
-        });
+        console.log(text);
+        console.log("GOT MESSAGE FROM SERVER", event);
+        createMessageElement(text, false);
+        scrollToBottomIfAtEnd();
     }
 
     document.getElementById("message-form").addEventListener("submit", function (e) {
@@ -45,19 +37,37 @@ function sendmessage() {
     let messageInput = document.getElementById("message-input")
     console.log("Sending text ", messageInput.value)
     if (messageInput.value) {
-        socket.send(messageInput.value)
-
-        let newNode = document.createElement("div")
-        newNode.classList.add("message", "message-self")
-        newNode.textContent = messageInput.value
-        document.getElementById("messages").appendChild(newNode)
-
+        socket.send(messageInput.value);
+        createMessageElement(messageInput.value, true);
         messageInput.value = "";
 
-        const messagesDiv = document.getElementById('messages');
-        messagesDiv.scrollTo({
-            top: messagesDiv.scrollHeight,
-            behavior: 'smooth'
-        });
+        // Always scroll to bottom if the user is sending a message
+        scrollToBottom();
     }
+}
+
+function scrollToBottomIfAtEnd() {
+    const messagesDiv = document.getElementById('messages');
+
+    const isNearBottom =
+        messagesDiv.scrollTop + messagesDiv.clientHeight >= messagesDiv.scrollHeight - SCROLL_MESSAGES_DISTANCE;
+
+    if (isNearBottom) {
+        scrollToBottom();
+    }
+}
+
+function scrollToBottom() {
+    const messagesDiv = document.getElementById('messages');
+    messagesDiv.scrollTo({
+        top: messagesDiv.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+
+function createMessageElement(messageContent, isSelf) {
+    let newNode = document.createElement("div")
+    newNode.classList.add("message", isSelf ? "message-self" : "message-other")
+    newNode.textContent = messageContent
+    document.getElementById("messages").appendChild(newNode)
 }
