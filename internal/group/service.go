50a76203f8939f06fcbc19ce2de7690907e688e4
ananthvk <mailstoanantha@gmail.com>
2025-11-14 23:04:26 +0530

    feat: implement group create, update, delete, and fetch

--- internal/group/service.go
+++ internal/group/service.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ananthvk/gochat/internal/database"
 	"github.com/ananthvk/gochat/internal/database/db"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/oklog/ulid/v2"
 )
 
@@ -53,14 +54,51 @@ func (g *GroupService) GetOne(ctx context.Context, public_id ulid.ULID) (*db.Grp
 	return grp, nil
 }
 
-func (g *GroupService) Delete(ctx context.Context) {
-	slog.Info("deleted a group")
+func (g *GroupService) Delete(ctx context.Context, public_id ulid.ULID) error {
+	ctx, cancel := context.WithTimeout(ctx, g.Db.QueryTimeout)
+	defer cancel()
+	err := g.Db.Queries.DeleteGroupByPublicId(ctx, public_id[:])
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			slog.ErrorContext(ctx, "internal error while fetching group", "error", err)
+			return errors.New("internal server error")
+		}
+	}
+	return nil
 }
 
-func (g *GroupService) Update(ctx context.Context) {
-	slog.Info("updated a group")
+func deref(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
 }
 
-func (g *GroupService) GetAll(ctx context.Context) {
-	slog.Info("retrieved all groups for this user")
+// Updates a group (supports partial updates)
+func (g *GroupService) Update(ctx context.Context, public_id ulid.ULID, req GroupUpdateRequest) (*db.Grp, error) {
+	ctx, cancel := context.WithTimeout(ctx, g.Db.QueryTimeout)
+	defer cancel()
+	group, err := g.Db.Queries.UpdateGroupByPublicId(ctx, db.UpdateGroupByPublicIdParams{
+		Name:        pgtype.Text{String: deref(req.Name), Valid: req.Name != nil},
+		Description: pgtype.Text{String: deref(req.Description), Valid: req.Description != nil},
+		PublicID:    public_id[:],
+	})
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			slog.ErrorContext(ctx, "internal error while updating group", "error", err)
+			return nil, errors.New("internal server error")
+		}
+		return nil, errors.New("group with the given id not found")
+	}
+	return group, nil
+}
+
+func (g *GroupService) GetAll(ctx context.Context) ([]*db.Grp, error) {
+	ctx, cancel := context.WithTimeout(ctx, g.Db.QueryTimeout)
+	defer cancel()
+	grps, err := g.Db.Queries.GetGroups(ctx)
+	if err != nil {
+		return nil, errors.New("error while fetching records")
+	}
+	return grps, nil
 }
