d2ad070a6663a13de438df77d1ee99eaa62d994a
ananthvk <mailstoanantha@gmail.com>
2025-11-26 23:34:46 +0530

    feat: add frontend vite project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <h1 className="text-3xl font-bold underline p-6 hover:bg-sky-800">
+            Hello world!
+        </h1>
+    )
+
+}
+
+export default App
